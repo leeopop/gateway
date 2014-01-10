469e57f8074ab19d19b92d7644ebfe820f793eb9
leeopop <dlrmsghd@gmail.com>
2014-01-10 22:03:21 +0900

    dpdk run success

--- gateway/gateway.cpp
+++ gateway/gateway.cpp
@@ -6,11 +6,13 @@
  */
 
 #include <iostream>
+#include <rte_eal.h>
 
 using namespace std;
 
 int main(int argc, char** argv)
 {
-	cout<<"Hello world!"<<endl;
+	int ret = rte_eal_init(argc, argv);
+	cout<<ret<<endl;
 	return 0;
 }
