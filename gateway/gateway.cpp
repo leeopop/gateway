0d2d5bb62ce0f7b99b8a972d3aa521d2a0e7825c
leeopop <dlrmsghd@gmail.com>
2014-01-10 21:31:30 +0900

    c++ path include

---
+++ gateway/gateway.cpp
@@ -0,0 +1,16 @@
+/*
+ * gateway.cpp
+ *
+ *  Created on: 2014. 1. 10.
+ *      Author: leeopop
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout<<"Hello world!"<<endl;
+	return 0;
+}
